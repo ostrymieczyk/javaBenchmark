72323d7444c365ebf58e91f1878fd3c8b10d4969
ostrymieczyk <mondii668@gmail.com>
2016-11-05 19:25:51 +0100

    Test UI added

--- javaBench/src/GUI/Main.java
+++ javaBench/src/GUI/Main.java
@@ -1,10 +1,30 @@
 package GUI;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+@SuppressWarnings("restriction")
+public class Main extends Application {
 
-	}
+    public Main() {
+        super();
+    }
 
+
+    public static void main(String[] args)  {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        primaryStage.setTitle("Java Benchmark");
+        Pane myPane = (Pane)FXMLLoader.load(getClass().getResource("Main.fxml"));
+        Scene myScene = new Scene(myPane);
+        primaryStage.setScene(myScene);
+        primaryStage.show();
+    }
 }
